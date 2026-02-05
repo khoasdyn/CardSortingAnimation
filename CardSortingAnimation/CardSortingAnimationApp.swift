86d4842f35101f68ee393614ca3b51243800e035
khoasdyn <khoasdyn@gmail.com>
2026-02-05 15:43:52 +0700

    Initial Commit

---
+++ CardSortingAnimation/CardSortingAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardSortingAnimationApp.swift
+//  CardSortingAnimation
+//
+//  Created by khoasdyn on 5/2/26.
+//
+
+import SwiftUI
+
+@main
+struct CardSortingAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
